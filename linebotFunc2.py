c2d3ae00970d6d1e4c7e8f03a82c85736efa4162
DoubleKai-1230 <137135910+DoubleKai-1230@users.noreply.github.com>
2024-05-01 15:33:28 +0800

    Add files via upload

---
+++ linebotFunc2.py
@@ -0,0 +1,67 @@
+from flask import Flask
+app = Flask(__name__)
+
+from flask import request, abort
+from linebot import  LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, AudioSendMessage, VideoSendMessage,ImageSendMessage,StickerSendMessage
+
+line_bot_api = LineBotApi(os.environ.get('Channel_Access_Token'))
+handler = WebhookHandler(os.environ.get('Channel_Secret'))
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+baseurl = 'https://linebutfunction1.onrender.com/static/'  #靜態檔案網址
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    mtext = event.message.text
+    if mtext == '@傳送聲音':
+        try:
+            message = AudioSendMessage(
+                original_content_url=baseurl + 'mario.m4a',  #聲音檔置於static資料夾
+                duration=20000  #聲音長度20秒
+            )
+            line_bot_api.reply_message(event.reply_token, message)
+        except:
+            line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
+
+    elif mtext == '@傳送影片':
+        try:
+            message = VideoSendMessage(
+                original_content_url=baseurl + 'robot.mp4',  #影片檔置於static資料夾
+                preview_image_url=baseurl + 'robot.jpg'
+            )
+            line_bot_api.reply_message(event.reply_token, message)
+        except:
+            line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
+
+    elif mtext == '@多項傳送':
+        try:
+            message = [  #串列
+                StickerSendMessage(  #傳送貼圖
+                    package_id='1',  
+                    sticker_id='2'
+                ),
+                TextSendMessage(  #傳送文字
+                    text = "這是 Pizza 圖片！"
+                ),
+                ImageSendMessage(  #傳送圖片
+                    original_content_url = "https://i.imgur.com/4QfKuz1.png",
+                    preview_image_url = "https://i.imgur.com/4QfKuz1.png"
+                )
+            ]
+            line_bot_api.reply_message(event.reply_token,message)
+        except:
+            line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
+
+if __name__ == '__main__':
+    app.run()
