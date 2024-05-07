ce3163c8904355c1247d86834981be92cf61fe6c
DoubleKai-1230 <137135910+DoubleKai-1230@users.noreply.github.com>
2024-05-08 03:56:55 +0800

    Update linebotFunc2.py

--- linebotFunc2.py
+++ linebotFunc2.py
@@ -1,14 +1,16 @@
 from flask import Flask
 app = Flask(__name__)
 
-import os
 from flask import request, abort
 from linebot import  LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage, AudioSendMessage, VideoSendMessage,ImageSendMessage,StickerSendMessage
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, TemplateSendMessage, ConfirmTemplate, MessageTemplateAction, ButtonsTemplate, URITemplateAction 
+import random
 
 line_bot_api = LineBotApi(os.environ.get('Channel_Access_Token'))
 handler = WebhookHandler(os.environ.get('Channel_Secret'))
+
+game = ['剪刀','石頭','布'] #定義一個想要隨機回覆的list
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -20,53 +22,98 @@ def callback():
         abort(400)
     return 'OK'
 
-baseurl = 'https://linebutfunction1.onrender.com/static/'  #靜態檔案網址
-
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     mtext = event.message.text
-    if mtext == '@傳送聲音':
-        try:
-            message = AudioSendMessage(
-                original_content_url=baseurl + 'mario.m4a',  #聲音檔置於static資料夾
-                duration=20000  #聲音長度20秒
-            )
-            line_bot_api.reply_message(event.reply_token, message)
-        except:
-            line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
+    if mtext == '@按鈕樣板':
+        sendButton(event)
 
-    elif mtext == '@傳送影片':
-        try:
-            message = VideoSendMessage(
-                original_content_url=baseurl + 'robot.mp4',  #影片檔置於static資料夾
-                preview_image_url=baseurl + 'robot.jpg'
-            )
-            line_bot_api.reply_message(event.reply_token, message)
-        except:
-            line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
+    elif mtext == '@確認樣板':
+        sendConfirm(event)
+    
+    elif mtext == '@購買披薩':
+        sendPizza(event)
 
-    elif mtext == '@多項傳送':
-        try:
-            message = [  #串列
-                TextSendMessage(  #傳送文字
-                    text = "這是聲音、影片還有貓貓~"
-                ),
-                AudioSendMessage(
-                    original_content_url=baseurl + 'mario.m4a',  #聲音檔置於static資料夾
-                    duration=20000  #聲音長度20秒
-                ),
-                VideoSendMessage(
-                    original_content_url=baseurl + 'robot.mp4',  #影片檔置於static資料夾
-                    preview_image_url=baseurl + 'robot.jpg'
-                ),
-                ImageSendMessage(  #傳送圖片
-                    original_content_url =baseurl + 'cat.jpg',
-                    preview_image_url =baseurl + 'cat.jpg'
-                )
-            ]
-            line_bot_api.reply_message(event.reply_token,message)
-        except:
-            line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
+    elif mtext == '@yes':
+        sendYes(event)
+
+    elif mtext == '玩遊戲':
+        sendButton(event)
+    elif mtext in ['剪刀','石頭','布']:
+        randomRespond(event)
+
+def sendButton(event):  #按鈕樣版
+    try:
+        message = TemplateSendMessage(
+            alt_text='按鈕樣板',
+            template=ButtonsTemplate(
+                thumbnail_image_url='https://i.imgur.com/4QfKuz1.png',  #顯示的圖片
+                title='按鈕樣版示範',  #主標題
+                text='請選擇：',  #副標題
+                actions=[
+                    MessageTemplateAction(  #顯示文字計息
+                        label='文字訊息',
+                        text='@購買披薩'
+                    ),
+                    URITemplateAction(  #開啟網頁
+                        label='連結網頁',
+                        uri='http://www.e-happy.com.tw'
+                    )
+                ]
+            )
+        )
+        line_bot_api.reply_message(event.reply_token, message)
+    except:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
+
+def sendConfirm(event):  #確認樣板
+    try:
+        message = TemplateSendMessage(
+            alt_text='確認樣板',
+            template=ConfirmTemplate(
+                text='你確定要購買這項商品嗎？',
+                actions=[
+                    MessageTemplateAction(  #按鈕選項
+                        label='是',
+                        text='@yes'
+                    ),
+                    MessageTemplateAction(
+                        label='否',
+                        text='@no'
+                    )
+                ]
+            )
+        )
+        line_bot_api.reply_message(event.reply_token, message)
+    except:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
+
+def sendPizza(event):
+    try:
+        message = TextSendMessage(
+            text = '感謝您購買披薩，我們將盡快為您製作。'
+        )
+        line_bot_api.reply_message(event.reply_token, message)
+    except:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
+
+def sendYes(event):
+    try:
+        message = TextSendMessage(
+            text='感謝您的購買，\n我們將盡快寄出商品。',
+        )
+        line_bot_api.reply_message(event.reply_token, message)
+    except:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
+        
+def randomRespond(event):
+    try:
+        message = TextSendMessage(
+            text=random.choice(game)
+        )
+        line_bot_api.reply_message(event.reply_token, message)
+    except:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
 
 if __name__ == '__main__':
     app.run()
