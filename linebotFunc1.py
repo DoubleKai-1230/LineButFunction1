8454b8737e8c914704b889d9058706f01c1ec64e
DoubleKai-1230 <137135910+DoubleKai-1230@users.noreply.github.com>
2024-11-29 12:34:56 +0800

    Update linebotFunc1.py

--- linebotFunc1.py
+++ linebotFunc1.py
@@ -1,16 +1,21 @@
 from flask import Flask
 app = Flask(__name__)
 
-from flask import request, abort
+from flask import request, abort, render_template
 from linebot import  LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, PostbackEvent, TextSendMessage, ImagemapSendMessage, BaseSize, MessageImagemapAction, URIImagemapAction, ImagemapArea, TemplateSendMessage, ButtonsTemplate, DatetimePickerTemplateAction
-from urllib.parse import parse_qsl
-import datetime
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, BubbleContainer, ImageComponent, BoxComponent, TextComponent, IconComponent, ButtonComponent, SeparatorComponent, FlexSendMessage, URIAction
+
 import os
 
 line_bot_api = LineBotApi(os.environ.get('Channel_Access_Token','5WCNMJ7+/ZVd9WrkSVDiYko+MgaIZZ1EtOF9e93Oz/+YdCPeH47bKiC1CB+VggLIsqGsRuNbg3WPGlRxTqYyMaEwGV5f3/SI3qldh4dhAlfJirACyEFMekaU75aKaU3iGY800NQ1dYIlol1KOMdPdgdB04t89/1O/w1cDnyilFU='))
 handler = WebhookHandler(os.environ.get('Channel_Secret','43c28d007bea14b83aeb96d957a9484b'))
+liffid = '2006619471-X4DVaD77'
+
+#LIFF靜態頁面
+@app.route('/page')
+def page():
+	return render_template('index.html', liffid = liffid)
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -25,107 +30,105 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     mtext = event.message.text
-    if mtext == '@圖片地圖':
-        sendImgmap(event)
+    if mtext == '@彈性配置':
+        sendFlex(event)
 
-    elif mtext == '@日期時間':
-        sendDatetime(event)
+    elif mtext[:3] == '###' and len(mtext) > 3:
+         manageForm(event, mtext)
 
-@handler.add(PostbackEvent)  #PostbackTemplateAction觸發此事件
-def handle_postback(event):
-    backdata = dict(parse_qsl(event.postback.data))  #取得data資料
-    if backdata.get('action') == 'sell':
-        sendData_sell(event, backdata)
-
-def sendImgmap(event):  #圖片地圖
+def sendFlex(event):  #彈性配置
     try:
-        image_url = 'https://i.imgur.com/Yz2yzve.jpg'  #圖片位址
-        imgwidth = 1040  #原始圖片寛度一定要1040
-        imgheight = 300
-        message = ImagemapSendMessage(
-            base_url=image_url,
-            alt_text="圖片地圖範例",
-            base_size=BaseSize(height=imgheight, width=imgwidth),  #圖片寬及高
-            actions=[
-                MessageImagemapAction(  #顯示文字訊息
-                    text='你點選了紅色區塊！',
-                    area=ImagemapArea(  #設定圖片範圍:左方1/4區域
-                        x=0, 
-                        y=0, 
-                        width=imgwidth*0.25, 
-                        height=imgheight  
-                    )
-                ),
-                URIImagemapAction(  #開啟網頁
-                    link_uri='http://www.e-happy.com.tw',
-                    area=ImagemapArea(  #右方1/4區域(藍色1)
-                        x=imgwidth*0.75, 
-                        y=0, 
-                        width=imgwidth*0.25, 
-                        height=imgheight  
-                    )
-                ),
-            ]
-        )
-        line_bot_api.reply_message(event.reply_token, message)
-    except:
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
-
-def sendDatetime(event):  #日期時間
-    try:
-        message = TemplateSendMessage(
-            alt_text='日期時間範例',
-            template=ButtonsTemplate(
-                thumbnail_image_url='https://i.imgur.com/VxVB46z.jpg',
-                title='日期時間示範',
-                text='請選擇：',
-                actions=[
-                    DatetimePickerTemplateAction(
-                        label="選取日期",
-                        data="action=sell&mode=date",  #觸發postback事件
-                        mode="date",  #選取日期
-                        initial="2024-11-15",  #顯示初始日期
-                        min="2024-11-15",  #最小日期
-                        max="2024-12-31"  #最大日期
-                    ),
-                    DatetimePickerTemplateAction(
-                        label="選取時間",
-                        data="action=sell&mode=time",
-                        mode="time",  #選取時間
-                        initial="10:00",
-                        min="00:00",
-                        max="23:59"
-                    ),
-                    DatetimePickerTemplateAction(
-                        label="選取日期時間",
-                        data="action=sell&mode=datetime",
-                        mode="datetime",  #選取日期時間
-                        initial="2024-11-15T13:00",
-                        min="2024-11-15T00:00",
-                        max="2024-12-31T23:59"
-                    )
+        bubble = BubbleContainer(
+            direction='ltr',  #項目由左向右排列
+            header=BoxComponent(  #標題
+                layout='vertical',
+                contents=[
+                    TextComponent(text='冰火飲料', weight='bold', size='xxl'),
                 ]
-            )
+            ),
+            hero=ImageComponent(  #主圖片
+                url='https://i.imgur.com/3sBRh08.jpg',
+                size='full',
+                aspect_ratio='792:555',  #長寬比例
+                aspect_mode='cover',
+            ),
+            body=BoxComponent(  #主要內容
+                layout='vertical',
+                contents=[
+                    TextComponent(text='評價', size='md'),
+                    BoxComponent(
+                        layout='baseline',  #水平排列
+                        margin='md',
+                        contents=[
+                            IconComponent(size='lg', url='https://i.imgur.com/GsWCrIx.png'),
+                            TextComponent(text='25   ', size='sm', color='#999999', flex=0),
+                            IconComponent(size='lg', url='https://i.imgur.com/sJPhtB3.png'),
+                            TextComponent(text='14', size='sm', color='#999999', flex=0),
+                        ]
+                    ),
+                    BoxComponent(
+                        layout='vertical',
+                        margin='lg',
+                        contents=[
+                            BoxComponent(
+                                layout='baseline',
+                                contents=[
+                                    TextComponent(text='營業地址:', color='#aaaaaa', size='sm', flex=2),
+                                    TextComponent(text='台北市信義路14號', color='#666666', size='sm', flex=5)
+                                ],
+                            ),
+                            SeparatorComponent(color='#0000FF'),
+                            BoxComponent(
+                                layout='baseline',
+                                contents=[
+                                    TextComponent(text='營業時間:', color='#aaaaaa', size='sm', flex=2),
+                                    TextComponent(text="10:00 - 23:00", color='#666666', size='sm', flex=5),
+                                ],
+                            ),
+                        ],
+                    ),
+                    BoxComponent(  
+                        layout='horizontal',
+                        margin='xxl',
+                        contents=[
+                            ButtonComponent(
+                                style='primary',
+                                height='sm',
+                                action=URIAction(label='電話聯絡', uri='tel:0987654321'),
+                            ),
+                            ButtonComponent(
+                                style='secondary',
+                                height='sm',
+                                action=URIAction(label='查看網頁', uri="http://www.e-happy.com.tw")
+                            )
+                        ]
+                    )
+                ],
+            ),
+            footer=BoxComponent(  #底部版權宣告
+                layout='vertical',
+                contents=[
+                    TextComponent(text='Copyright@ehappy studio 2019', color='#888888', size='sm', align='center'),
+                ]
+            ),
         )
+        message = FlexSendMessage(alt_text="彈性配置範例", contents=bubble)
         line_bot_api.reply_message(event.reply_token,message)
     except:
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
 
-def sendData_sell(event, backdata):  #Postback,顯示日期時間
+def manageForm(event, mtext):
     try:
-        if backdata.get('mode') == 'date':
-            dt = '日期為：' + event.postback.params.get('date')  #讀取日期
-        elif backdata.get('mode') == 'time':
-            dt = '時間為：' + event.postback.params.get('time')  #讀取時間
-        elif backdata.get('mode') == 'datetime':
-            dt = datetime.datetime.strptime(event.postback.params.get('datetime'), '%Y-%m-%dT%H:%M')  #讀取日期時間
-            dt = dt.strftime('{d}%Y-%m-%d, {t}%H:%M').format(d='日期為：', t='時間為：')  #轉為字串
+        flist = mtext[3:].split('/')
+        text1 = '姓名：' + flist[0] + '\n'
+        text1 += '日期：' + flist[1] + '\n'
+        text1 += '包廂：' + flist[2]
         message = TextSendMessage(
-            text=dt
+            text = text1
         )
         line_bot_api.reply_message(event.reply_token,message)
     except:
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
